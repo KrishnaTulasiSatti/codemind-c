bf1155cbc72afe43bb7d60f0766c56a11b5869c6
KrishnaTulasiSatti <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2023-11-19 11:23:08 +0530

    #include<stdio.h> int main()
    {
        int n;
        scanf("%d",&n);
        for(int i = 0;i <n ;i++)
        {
            for(int k = 0;k<i;k++)
            {
                printf(" ");
            }
            
            
            for(int j = n;j > i;j--)
            {
                printf("%c ",char(i+65));
            }
            
            printf("
    ");
        }
        
    }

---
+++ Pattern_7.5.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+
+{
+    int n;
+    scanf("%d",&n);
+    for(int i = 0;i <n ;i++)
+    {
+        for(int k = 0;k<i;k++)
+        {
+            printf(" ");
+        }
+        
+        
+        for(int j = n;j > i;j--)
+        {
+            printf("%c ",char(i+65));
+        }
+        
+        printf("
+");
+    }
+    
+}
