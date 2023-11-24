4b9dc846cac253a48fa765493e9c30cf8e45aeda
KrishnaTulasiSatti <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2023-11-24 20:51:52 +0530

    #include<stdio.h> #include<math.h> int main() {     int n;     scanf("%d",&n);     float fvar = sqrt(n);     int ivar = fvar;     if(ivar == fvar) printf("True");     else printf("False"); }

--- perfect_square_root_or_not.c
+++ perfect_square_root_or_not.c
@@ -1,26 +1,11 @@
 #include<stdio.h>
-int checkroot(int n,int i) 
-{
-    
-    if(i == (n/i)) return 1;
-    else return 0;
-}
+#include<math.h>
 int main()
 {
     int n;
     scanf("%d",&n);
-    int flag = 0;
-    if(n==1 || n==0) printf("True");
-    for(int i=1;i<n;i++) {
-        if(n % i == 0) {
-            if(checkroot(n,i)) 
-            {
-                flag = 1;
-                printf("True");
-                break;
-            }
-
-        }
-    }
-    if(flag != 1) printf("False");
+    float fvar = sqrt(n);
+    int ivar = fvar;
+    if(ivar == fvar) printf("True");
+    else printf("False");
 }
