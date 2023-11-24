cfdf5fd1589bbee5e4f476596bf79eafe8e5bb3c
KrishnaTulasiSatti <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2023-11-24 19:52:21 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n];     for(int i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int ans[n];     ans[n-1]=0;     for(int i=0;i<n-1;i++)     {         int c=0;         for(int j=i+1;j<n;j++)         {             c++;             if(arr[j]>arr[i])             {                 ans[i]=c;                 break;             }             else{                 ans[i]=0;             }         }     }     for(int i=0;i<n;i++)     {         printf("%d ",ans[i]);     } }

---
+++ Temperatures.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int ans[n];
+    ans[n-1]=0;
+    for(int i=0;i<n-1;i++)
+    {
+        int c=0;
+        for(int j=i+1;j<n;j++)
+        {
+            c++;
+            if(arr[j]>arr[i])
+            {
+                ans[i]=c;
+                break;
+            }
+            else{
+                ans[i]=0;
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",ans[i]);
+    }
+}
